8d07ade72c1688dcfc9401c4611ea21ca4d829b2
GaoShuYi <gaoshuyidaddy@foxmail.com>
2019-03-20 00:06:36 +0800

    First Commit

---
+++ src/main/java/com/quzeng/gc_gradle_demo/GcGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.quzeng.gc_gradle_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcGradleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcGradleDemoApplication.class, args);
+    }
+
+}
